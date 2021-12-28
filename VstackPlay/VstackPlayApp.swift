ee7ef91b159258151db09e963095d73e10461300
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-12-28 16:10:47 +0200

    Initial Commit

---
+++ VstackPlay/VstackPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  VstackPlayApp.swift
+//  VstackPlay
+//
+//  Created by Vahtee Boo on 28.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct VstackPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
